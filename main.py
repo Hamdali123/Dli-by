9d3435fdf32088dc117582803ae621a7881af010
hamdali123 <dali75943@gmail.com>
2024-10-05 15:05:47 +0700

    add repository

---
+++ main.py
@@ -0,0 +1,12 @@
+# mendefinisikan variabel
+nilai1 = 15
+nilai2= 10
+nilai3 = 20
+
+jawab1 = nilai2 == nilai3
+jawab2 = nilai1 >= nilai3
+jawab3 = nilai1 < nilai3 
+
+print ("jawaban pertanyaan:",jawab1)
+print ("jawaban pertanyaan:", jawab2)
+print ("jawaban pertanyaan:",  jawab3)
