9a725f4b15c0a2c9cbcb2a400a282e2549752a0a
hamdali123 <dali75943@gmail.com>
2024-10-19 11:31:16 +0700

    pertemuan5

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-# Mendefinisikan variabel
-nama = "Budi"
-umur = 25
-kota = "Jakarta"
+#sedang latihan perulangan FOR
+'''for i in range(20):
+    print("sedang belajar pyhton")
+    if i == 5:
+        break
+    
+#sedang belajar perulangan while
+
+
+while angka <= 15:
+    print(angka)
+    angka += 1'''
+    
+    
+    
+''' # PERCABANGAN IF ELSE
+nilai= 75
+
+if nilai >= 75:
+    print("anda lulus ujian.")
+else:
+    print("anda belum lulus ujian.")'''
+
+#belajar fungsi 
+
+        #mengisi milai parameter
+def sapa(nama, ):
+    print("hallo,",nama, "!")
+# Format string tanpa jarak spasi
+    print(f"hallo, {nama}!")
+# menampilkan hasil fungsi
+
+#mengisi nilai fungsi dnegan tipe data string yaitu andi
+sapa("andi")
+# output: halo,andi!
+
+
+def tambah (a,b):
+    hasil
---
+++ main2.py
@@ -0,0 +1,99 @@
+#belajar fungsi 
+
+        #mengisi milai parameter
+#def sapa(nama, ):
+#    print("hallo,",nama, "!")
+# Format string tanpa jarak spasi
+#    print(f"hallo, {nama}!")
+# menampilkan hasil fungsi
+
+#mengisi nilai fungsi dnegan tipe data string yaitu andi
+#sapa("andi")
+# output: halo,andi!
+
+
+# def tambah (a,b):
+#     hasil = a + b
+#     return hasil
+
+# jumlah = tambah(3, 5)
+#print("hasil penjumlahan adalah:", jumlah) # Output: 8
+
+
+
+# def tambah(a, b):
+#     hasil = a + b
+#     return hasil
+
+# jumlah = tambah(3, 5)
+# hasil_bagi = jumlah / 2
+# print("hasil penjumlahan adalah:", jumlah)  # Output: 8
+# print("hasil penjumlahan dibagi dua adalah:", hasil_bagi) 
+
+
+# def tambah(a, b, c):
+#     hasil =(a + b) / c
+#     return hasil
+
+# jumlah= tambah(3, 5, 2)
+# print("Hasil penjumlahan adalah:", jumlah) 
+#  # Output: 8:4=2
+
+
+#  # membuat list
+#  buah = ("apel","pisang","mangga")
+
+#  # mengkases elemen
+#  print (buah{0}) # output: apel
+
+#  # mengubah elemen
+#  buah{1}
+
+
+
+
+# buah = ["apel","pisang","mangga"]
+# buah[1] = "jeruk"
+# print(buah)
+
+
+
+# buah = ["apel", "jeruk", "mangga"]
+
+# # Menambahkan dua buah
+# buah += ["kiwi", "anggur"]
+
+# # menghapus mangga
+# buah.remove ("mangga")
+# print(buah)  # Output: ['apel', 'jeruk', 'kiwi', 'anggur'] 
+
+
+# # daftar nama buah
+# buah = ["apel","pisang","mangga"]
+
+# # mengakses elemen baru pada buah pada daftar list
+# buah = [1] ="jeruk"
+
+# # menambahkan list baru dengan index tertnetu 
+# buah.insert(0, "anggur")
+
+# # menambahkan buah
+# buah.appennd("alpukat")
+
+# # menghapus buah
+# buah.remove("mangga")
+# print(buah)
+
+
+
+# Struktur data Tuple
+
+angka1 = (1, 2, 3)
+angka2 = (4, 5, 6)
+
+angka3 = angka1+angka2
+
+print(angka3)
+print(angka1 + angka2)
+
+
