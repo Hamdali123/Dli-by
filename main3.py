e06e547e5a4061b589974aa000b29431679bdc0a
hamdali123 <dali75943@gmail.com>
2024-10-27 14:10:04 +0700

    pertemuan5

---
+++ main3.py
@@ -0,0 +1,148 @@
+# # membuat set
+# angka = {1,2,3,2,1,} #duplikat di hilangkan 
+# print(angka) # output: {1,2,3}
+
+# #operasi himpunan
+# himpunan_a = {1,2,3}
+# himpunan_b = {3,4,5,}
+# print(himpunan_a.union(himpunan_b)) #gabungan
+# print(himpunan_a.intersection(himpunan_b)) #Irisan 
+# 
+# 
+# 
+# 
+# 
+# latihan yang bawah
+#  
+
+
+
+# periksa apakah nilai variabel umur lebih besar dari 18 
+
+# umur1 = 18
+# umur2 = 20
+# print(umur2 > umur1)
+
+
+'''# #periksa apakah nilai variabel sama dengan "andi"
+
+
+
+nama1 = "budi"
+nama2 = "andi"
+
+print(nama1 == nama2)
+'''
+
+
+
+
+
+# if else elseif 
+# buatlah program yang minta input nomor hari (1-7) lalu menampilkan nama hari _hari.
+
+'''# Meminta input nomor hari
+nomor_hari = int(input("Masukkan nomor hari (1-7): "))
+
+# Menentukan nama hari berdasarkan nomor
+if nomor_hari == 1:
+    print("Hari: Senin")
+elif nomor_hari == 2:
+    print("Hari: Selasa")
+elif nomor_hari == 3:
+    print("Hari: Rabu")
+elif nomor_hari == 4:
+    print("Hari: Kamis")
+elif nomor_hari == 5:
+    print("Hari: Jumat")
+elif nomor_hari == 6:
+    print("Hari: Sabtu") # elif fungsinya untuk mengecek tambahan saja
+
+elif nomor_hari == 7:
+    print("Hari: Minggu")
+else: # else fungsinya yang tidak valid
+    print("Nomor tidak valid! Silakan masukkan nomor antara 1 dan 7.")''' 
+
+
+
+
+# contoh dari mas riski
+'''# opertor perbandingan
+# soal: periksa apakah nilai 
+
+# variabel umur lebih besar dari 18.
+
+umur = 18
+is_dewasa = umur > 18
+print(is_dewasa) # Output:'''
+
+
+'''#periksa apakah nilai variabel sama dengan "andi"
+
+nama = "budi"
+
+is_andi = nama == "andi"
+
+print (is_andi) # output:'''
+
+
+
+'''c'''
+   
+'''#soal no4
+
+def hitung_luas_persegi_panjang(panjang, lebar)
+
+luas = panjang*lebar
+return luas
+
+panjang_persegi_panjang = 10
+lebar_persegi_panjang = 5
+
+hasil_luas  = hitung_luas_persegi_panjang (panjang_persegi panjang lebar perseg)
+
+print (luas persegi panjang: hasil luas)'''
+
+
+#soal no5
+
+buah = ["apel", "pisang", "mangga"]
+buah_tuple = tuple(buah)
+print(buah_tuple)
+
+print(buah-tuple[0])
+print(buah-tuple[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
